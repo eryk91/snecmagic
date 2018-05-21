56b9a91f4021c177d58b72a7998c363bff66011c
eryk91 <35232677+eryk91@users.noreply.github.com>
2018-05-21 20:00:09 +0200

    THX
    JW

--- index.php
+++ index.php
@@ -17,12 +17,12 @@
 </head>
 <body>
   <div id="preloader"><div id="preloader-circle"></div></div>
-  <header>
+  <header class="slide0">
     <h1>SNEC<span>Studio</span></h1>
     <p>Zespół tworzący profesjonalne strony internetowe z pasją</p>
     <div class="scrollArrow" id="scrollArrow" onclick="lipa()"></div>
-</div>
   </header>
+
   <section class="slide1">
     <h2>Kim jesteśmy?</h2>
     <p>
@@ -40,14 +40,86 @@
       <div class="circle">Prepare</div>
       <div class="circle">Publish</div>
     </div>
+    <div class="scrollArrow1" id="scrollArrow1" onclick="lipa1()"></div>
   </section>
+
   <section class="slide2">
     <h2>Zobacz nasze dotychczasowe projekty</h2>
+    <div id="overwrap">
+      <div class="square">First Example</div>
+      <div class="square">Second Example</div>
+      <div class="square">Third Example</div>
+    </div>
+    <div class="scrollArrow2" id="scrollArrow2" onclick="lipa2()"></div>
   </section>
+
   <section class="slide3">
-    <h2>Napisz do nas</h2>
+   <?php
+
+   ini_set('SMTP', 'ssl://smtp.gmail.com');
+   ini_set('SMTP', 'smtp.gmail.com');
+   ini_set('smtp_port', '465');
+   ini_set('username', 'snecstudio@gmail.com');
+   ini_set('password', 'gcaz&muak');
+   ini_set('sendmail_from', 'snecstudio@gmail.com');
+
+   if (!empty($_REQUEST['email']) and !empty($_REQUEST['tytul']) and !empty($_REQUEST['info'])) {
+
+       $admin_email = "snecstudio@gmail.com";
+       $email       = $_REQUEST['email'];
+       $tytul       = $_REQUEST['tytul'];
+       $info        = $_REQUEST['info'];
+
+       if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+           mail($admin_email, "'$tytul' Od: '$email'", $info);
+           echo "<h3>Dziękujemy za wiadomość!</h3>";
+       }
+
+       else {
+   ?>
+           <h2>Napisz do nas</h2>
+       <form id="mail" method="post">
+
+         <input name="email" type="text" placeholder="Twój e-mail:" /> <br />
+         <input name="tytul" type="text" placeholder="Tytuł:" /> <br />
+         <textarea name="info" placeholder="Twoja wiadomość do nas:"></textarea>
+
+         <input type="submit" value="Wyślij" />
+         <div style="clear: both"></div>
+         <div id="left">E-mail: <br /><a href="mailto:snecstudio@gmail.com">snecstudio@gmail.com</a></div>
+         <div id="right">Kontakt telefoniczny: <br /> +48 070 000 000</div>
+         <div style="clear: both"></div>
+       </form>
+    <?php
+       }
+   }
+
+   else {
+   ?>
+       <h2>Napisz do nas</h2>
+       <form id="mail" method="post">
+
+         <input name="email" type="text" placeholder="Twój e-mail" /> <br />
+         <input name="tytul" type="text" placeholder="Tytuł" /> <br />
+         <textarea name="info" placeholder="Wiadomość do nas."></textarea>
+
+         <input type="submit" value="Wyślij" />
+         <div style="clear: both"></div>
+         <div id="left">E-mail: <br /><a href="mailto:snecstudio@gmail.com">snecstudio@gmail.com</a></div>
+         <!--<div id="right">Kontakt telefoniczny: <br /> +48 000 000 000</div>-->
+         <div style="clear: both"></div>
+       </form>
+
+       <?php
+   }
+   ?>
+    <div class="scrollArrow3" id="scrollArrow3" onclick="lipa3()"></div>
   </section>
-  <footer>
-  </footer>
+  <footer >
+	<div>
+		<a href="mailto:snecstudio@gmail.com">Powered by <b style="color: #e2810a">SNEC</b><b style="color:#e2810a;text-decoration:unset;font-size: unset;">Studio</b> © <?php echo date("Y"); ?></a>
+	</div>
+</footer>
 </body>
 </html>
--- js/functions.js
+++ js/functions.js
@@ -27,7 +27,26 @@ jQuery(window).on('scroll', function($){
 
 function lipa() {
 $('#scrollArrow').click(function(){
-	$("html,body").animate({  scrollTop: $(".slide1").offset().top}, 998);
+	$("html,body").animate({  scrollTop: $(".slide1").offset().top}, 598);
+
+});
+}
+function lipa1() {
+$('#scrollArrow1').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide2").offset().top}, 598);
+
+});
+}
+function lipa2() {
+$('#scrollArrow2').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide3").offset().top}, 598);
+
+});
+}
+
+function lipa3() {
+$('#scrollArrow3').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide0").offset().top}, 898);
 
 });
 }
