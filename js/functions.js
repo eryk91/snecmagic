2eee7de163458a60cad5048cf0066ea53606ed79
Procesorek <propethrol@gmail.com>
2018-05-22 22:38:32 +0200

    ns
    xd

--- js/functions.js
+++ js/functions.js
@@ -5,13 +5,11 @@ $(document).ready(function() {
 });
 
 function deletePreloader() {
-  'use strict';
   $("div#preloader").remove();
   //$('html, body').css('overflowY', 'auto');
 }
 
 window.onbeforeunload = function () {
-  'use strict';
   window.scrollTo(0, 0);
 }
 
@@ -24,36 +22,8 @@ jQuery(window).on('scroll', function($){
   }
 });
 
-
-function lipa() {
-$('#scrollArrow').click(function(){
-	$("html,body").animate({  scrollTop: $(".slide1").offset().top}, 598);
-
-});
-}
-function lipa1() {
-$('#scrollArrow1').click(function(){
-	$("html,body").animate({  scrollTop: $(".slide2").offset().top}, 598);
-
-});
-}
-function lipa2() {
-$('#scrollArrow2').click(function(){
-	$("html,body").animate({  scrollTop: $(".slide3").offset().top}, 598);
-
-});
-}
-
-function lipa3() {
-$('#scrollArrow3').click(function(){
-	$("html,body").animate({  scrollTop: $(".slide0").offset().top}, 898);
-
-});
-}
-
 /* SCROLL MACHINE */
 var x = 0;
-var pos = $(".slide0").position();
 $('html').bind('mousewheel', function(e) {
     if(e.originalEvent.wheelDelta /120 < 0) {
       x+=0.5;
@@ -61,16 +31,47 @@ $('html').bind('mousewheel', function(e) {
     else{
       x-=0.5;
     }
-
+    if(x>=0 && x<=3) {
+      if(Number.isInteger(x)) $("html,body").animate({scrollTop: $(".slide"+x).offset().top}, 400);
+    }
     if(x>3) x=3;
     if(x<0) x=0;
     /*start slides effects */
-    pos = $(".slide"+x).position();
     //if(Number.isInteger(x)) $("body").css({"transform":"translateY(-"+x*100+"vh)"});
-    if(x>=0 && x<=3) {
-      if(Number.isInteger(x)) $("html,body").animate({scrollTop: $(".slide"+x).offset().top}, 400);      
-    }
+
 });
+
+
+
+function lipa() {
+$('#scrollArrow').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide1").offset().top}, 598);
+  x=1;
+
+});
+}
+function lipa1() {
+$('#scrollArrow1').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide2").offset().top}, 598);
+  x=2;
+
+});
+}
+function lipa2() {
+$('#scrollArrow2').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide3").offset().top}, 598);
+  x=3;
+});
+}
+
+function lipa3() {
+$('#scrollArrow3').click(function(){
+	$("html,body").animate({  scrollTop: $(".slide0").offset().top}, 898);
+  x=0;
+
+});
+}
+
 
 
 /*mobilescroll*/
